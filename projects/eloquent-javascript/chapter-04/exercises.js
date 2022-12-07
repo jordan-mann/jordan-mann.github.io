f9c0d11d45b8e67a428dca6227bad8ad997701f3
jordan-mann <jr.mannsjn@gmail.com>
2022-12-07 20:15:11 +0000

    first commit

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -2,7 +2,38 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function range() {
+function range(start, end, step) {
+  //create empty array
+  let output = [];
+  //test if start equals end
+  if (start === end) {
+    //return empty array
+    return output;
+  }
+  //test if step is negative
+  if (step < 0) {
+    //return empty array
+    return output;
+  }
+  //test if step is not given
+  if (step === undefined) {
+    //loop through range, incrementing by 1
+    for (let i = start; i <= end; i++) {
+      //push i into output
+      output.push(i);
+    }
+    //else step is given
+  }else {
+    //loop through array. Increment by step
+    for (let i = start; i <= end; i += step) {
+      //push i into output
+      output.push(i);
+    }
+  }
+  //return output
+  return output;
+  
+  
 
 }
 
@@ -10,24 +41,40 @@ function range() {
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function sum() {
+function sum(array, total = 0) {
+  if (array.length === 0) {
+    return total;
+  }
 
+  total += array[0];
+  return sum(array.slice(1), total);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArray ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArray() {
-
+function reverseArray(array, newArray = []) {
+if (array.length === 0) {
+  return newArray;
+} 
+newArray.unshift(array[0]);
+return reverseArray(array.slice(1), newArray);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // reverseArrayInPlace /////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function reverseArrayInPlace() {
+function reverseArrayInPlace(array) {
+// newArray = [];
+// for (let i = 0; i < array.length; i++) {
+//   newArray.push(array[i]);
+//   array.unshift(newArray[i]);
+// }
+// return array;
 
+return array.reverse();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
